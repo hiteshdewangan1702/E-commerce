28522256084d9ffd3738faa3dd3234dfeecef5f6
unknown <hiteshdewangan3777@gmail.com>
2024-05-17 15:02:54 +0530

    third

--- js/product.js
+++ js/product.js
@@ -24,34 +24,62 @@ const displayData = (data) => {
     leftDiv.classList.add("leftDiv")
     // leftDiv.style.display = "flex";
     const image = document.createElement('img')
+    image.setAttribute("id", "mimg")
+    image.style.height = "313px"
+    image.style.width = "558px"
     image.src = data.thumbnail;
     image.alt = data.title;
+
     console.log(data.thumbnail)
     const imgDiv = document.createElement("div")
-    imgDiv.setAttribute("id","nimg")
+    imgDiv.setAttribute("id", "nimg")
     data.images.forEach((i) => {
         const image1 = document.createElement("img")
         image1.src = i
         image1.alt = data.title
         image1.style.height = "50px"
         imgDiv.appendChild(image1)
+        image1.addEventListener("click", () => {
+            image.src = image1.src
+        })
     })
 
     leftDiv.append(imgDiv, image)
 
     const rightDiv = document.createElement("div")
+    rightDiv.setAttribute("id", "rightDiv")
     const title = document.createElement("h2")
     title.textContent = data.title
     const link = document.createElement("a")
     link.href = `./product.html?pid=${data.id}`
     link.target = "_blank"
+    link.style.textDecoration = "none";
     link.appendChild(title)
+
+    const desc = document.createElement("p")
+    desc.textContent = data.description;
+
+    const rating = document.createElement("p")
+    rating.textContent = "Rating " + data.rating
+    rating.style.fontWeight = "600"
+    rating.style.fontSize = "20px"
+
+
+    if (data.rating > 4.5) {
+        rating.style.color = "green"
+    }
+
+
     const price = document.createElement("p")
-    price.textContent = "$ " + data.price
+    price.textContent = "Price $ " + data.price
+    // price.style.color = "#FFEB3B"
+    price.style.fontWeight = "600"
+    price.style.fontSize = "20px"
 
     const addCart = document.createElement("button")
+    addCart.setAttribute("id", "btn2")
     addCart.textContent = "Add To Cart"
-    rightDiv.append(link, price, addCart)
+    rightDiv.append(link, desc, rating, price, addCart)
     product.append(leftDiv, rightDiv)
 }
 
